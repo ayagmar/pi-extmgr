e8987c2d15f360b0733e123d16b6044f68f43bf4
Abdeslam Yassine Agmar <abdeslamagmar@gmail.com>
2026-02-06 17:46:44 +0100

    zz

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...(tseslint.configs["recommended-type-checked"]?.rules ?? {}),
+      ...(tseslint.configs.stylistic?.rules ?? {}),
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { "prefer": "type-imports", "fixStyle": "inline-type-imports" }
+      ],
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+      ]
+    },
+  },
+];
